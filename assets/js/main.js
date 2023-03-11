19dc3da54088665444f142b84ed54c365da53c74
Omonjon <omonjons077@gmail.com>
2023-03-11 16:53:44 +0500

    special section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,6 +67,367 @@ darkModeToggle.addEventListener("click", () => {
         disableDarkMode();
     }
 });
+
+
+/* ********************************************************** */
+/* ########################################################## */
+/* Voice on mode ******************************************** */
+/* Modal okna zvukovoy effect uchun ************************* */
+let Voicemode = localStorage.getItem("Voicemode");
+var voiceTop = document.querySelector(".voiceOn");
+var timeCloseModalAlert = 1000;
+let voiceOn = false;
+let voiceCheck = true;
+var voiceModeControl = document.querySelector(".voiceModeControl");
+const voiceModeToggle = document.querySelector(".mode__voice");
+let voiceSaveToggle = document.querySelector(".voicemode input[type='checkbox']");
+
+/* Barcha matnlarni ovozlarni ******************************* */
+/* ijro etuvchi funksiyalar shuni ichida ******************** */
+function ifActiveVoice() {
+    if (!voiceOn) {
+        // Malum secundan keyin sahifa yangilanadi
+        setInterval(() => {
+            location.reload();
+        }, timeCloseModalAlert);
+    } else if (voiceOn) {
+        // A simple IIFE function.
+        // Простая функция IIFE.
+        // Oddiy IIFE funktsiyasi.
+        (function () {
+            "use strict"; // For the sake of practice.
+            // Ради практики.
+            // Amaliyot uchun.
+
+            if (typeof speechSynthesis === 'undefined') return; // Some config stuffs...
+            // Некоторые конфиги...
+            // Ba'zi konfiguratsiyalar ...
+
+            var voiceSelect = document.getElementById("voiceSelect");
+            var myPhrase = ' ';
+            var voices = []; // This is essentially similar to jQuery's $.ready.
+            // По сути, это похоже на $.ready в jQuery.
+            // Bu mohiyatan jQuery $.ready dasturiga o'xshaydi.
+
+            var ready = function (callback) {
+                var d = document,
+                    s = d.readyState; // DOMContentLoaded was fired
+                // DOMContentLoaded был запущен
+                // DOMContentLoaded ishdan bo'shatildi
+
+                if (s === "complete" || s === "loaded" || s === "interactive") {
+                    callback();
+                } else {
+                    if (d.addEventListener) {
+                        d.addEventListener("DOMContentLoaded", callback, false);
+                    } else {
+                        d.attachEvent("onDOMContentLoaded", callback);
+                    }
+                }
+            }; // This is a function to display all possible voice options.
+            // Это функция для отображения всех возможных вариантов голоса.
+            // Bu barcha mumkin bo'lgan ovozli variantlarni ko'rsatish funksiyasi.
+
+
+            function populateVoiceList() {
+                voices = speechSynthesis.getVoices();
+
+                for (var i = 0; i < voices.length; i++) {
+                    var option = document.createElement('option');
+                    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+                    option.textContent += voices[i].default ? ' -- DEFAULT' : '';
+                    option.setAttribute('data-lang', voices[i].lang);
+                    option.setAttribute('data-name', voices[i].name);
+                    document.getElementById("voiceSelect").appendChild(option);
+                }
+            } // This is the handler for when the select tag is changed.
+            // Это обработчик изменения тега select.
+            // Bu tanlangan teg o'zgartirilganda ishlov beruvchidir.
+
+
+            function handler() {
+                var utterThis = new SpeechSynthesisUtterance(myPhrase);
+                var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+                for (var i = 0; i < voices.length; i++) {
+                    if (voices[i].name === selectedOption) {
+                        utterThis.voice = voices[i];
+                    }
+                }
+
+                speechSynthesis.speak(utterThis);
+            };
+
+            function buildBtn(e) {
+                let button = document.getElementById("voice");
+                if (!getSelectionText()) {
+                    button.style.display = "none";
+                } else if (getSelectionText()) {
+                    let x = e.pageX;
+                    let y = e.pageY;
+                    let justBitPlus = -20;
+                    let justBitMinus = -5;
+                    button.style.display = "block";
+                    button.style.position = "absolute";
+                    button.style.left = justBitPlus + x + "px";
+                    button.style.top = justBitPlus + y + "px";
+                    /*button.style.left = justBitMinus + x + "px";
+                     button.style.top = justBitMinus + y + "px";*/
+
+                    /*
+                    let coor = "Coordinates: (" + x + "," + y + ")";
+                    button.innerHTML = coor;
+                    button.style.left = justBit + x + "px";
+                    button.style.top = justBit + y + "px";
+                    */
+                }
+
+            }
+
+            function fnBrowserDetect() {
+                let userAgent = navigator.userAgent;
+                let browserName;
+                var button = document.querySelector(".btn-voice");
+                var detectBrowser = document.querySelector(".detect");
+
+                if (userAgent.match(/chrome|chromium|crios/i)) {
+                    button.addEventListener("click", () => {
+                        setTimeout(function () {
+                            speechSynthesis.cancel();
+                            myPhrase = getSelectionText();
+                            handler();
+                        }, 1);
+                    });
+                    console.log("chrome work");
+                } else if (userAgent.match(/firefox|fxios/i)) {
+                    setTimeout(function () {
+                        speechSynthesis.cancel();
+                        myPhrase = getSelectionText();
+                        handler();
+                    }, 1);
+                } else if (userAgent.match(/safari/i)) {
+                    setTimeout(function () {
+                        speechSynthesis.cancel();
+                        myPhrase = getSelectionText();
+                        handler();
+                    }, 1);
+                } else if (userAgent.match(/opr\//i)) {
+                    button.addEventListener("click", () => {
+                        setTimeout(function () {
+                            speechSynthesis.cancel();
+                            myPhrase = getSelectionText();
+                            handler();
+                        }, 1);
+                    });
+                } else if (userAgent.match(/edg/i)) {
+                    button.addEventListener("click", () => {
+                        setTimeout(function () {
+                            speechSynthesis.cancel();
+                            myPhrase = getSelectionText();
+                            handler();
+                        }, 1);
+                    });
+                } else {
+                    button.addEventListener("click", () => {
+                        setTimeout(function () {
+                            speechSynthesis.cancel();
+                            myPhrase = getSelectionText();
+                            handler();
+                        }, 1);
+                    });
+                }
+            } // This is your code to get the selected text.
+            // Это ваш код для получения выделенного текста.
+            // Bu tanlangan matnni olish uchun sizning kodingiz.
+
+
+            function getSelectionText() {
+                var text = "";
+
+                if (window.getSelection) {
+                    text = window.getSelection().toString().trim(); // for Internet Explorer 8 and below. For Blogger, you should use &amp;&amp; instead of &&.
+                    // для Internet Explorer 8 и ниже. Для Blogger следует использовать &amp;&amp; вместо &&.
+                    // Internet Explorer 8 va undan past versiyalar uchun. Blogger uchun siz &amp;&amp;amp;amp;amp; ning o'rniga &&.
+                } else if (document.selection && document.selection.type !== "Control") {
+                    text = document.selection.createRange().text;
+                }
+
+                return text;
+            } // This is the on mouse up event, no need for jQuery to do this.
+            // Это событие при наведении мыши, для этого не требуется jQuery.
+            // Bu sichqonchani yuqoriga ko'tarish hodisasi, buni amalga oshirish uchun jQuery kerak emas.
+
+
+            document.onmouseup = function (e) {
+                fnBrowserDetect();
+                buildBtn(e);
+            }; // Some place for the application to start.
+            // Некоторое место для запуска приложения.
+            // Ilovani boshlash uchun ba'zi joy.
+
+
+            function start() {
+                populateVoiceList();
+                if (speechSynthesis.onvoiceschanged !== undefined) speechSynthesis.onvoiceschanged = populateVoiceList;
+                voiceSelect.onchange = handler;
+                setTimeout(handler, 75);
+            } // Run the start function.
+            // Запускаем функцию запуска.
+            // Boshlash funksiyasini ishga tushiring.
+
+            ready(start);
+
+
+        })();
+    }
+}
+const enableVoiceMode = () => {
+    voiceTop.classList.add("active");
+    voiceModeControl.classList.add("active");
+    voiceModeToggle.classList.add("active");
+    voiceSaveToggle.setAttribute("checked", "true");
+    voiceOn = true;
+    voiceCheck = true;
+    ifActiveVoice();
+    localStorage.setItem("Voicemode", "enabled");
+};
+const DisableAllSound = () => {
+    voiceModeControl.innerHTML = "";
+    document.getElementById("voice").classList.add("d-none", "visibility-hidden", "user-select");
+    /*document.getElementById("voice").innerHTML = "";*/
+}
+const EnableAllSound = () => {
+    document.getElementById("voice").classList.remove("d-none", "visibility-hidden", "user-select");
+}
+const disableVoiceMode = () => {
+    voiceTop.classList.remove("active");
+    voiceModeControl.classList.remove("active");
+    voiceModeToggle.classList.remove("active");
+    voiceSaveToggle.setAttribute("checked", "false");
+    voiceOn = false;
+    voiceCheck = false;
+    localStorage.setItem("Voicemode", null);
+};
+if (Voicemode === 'enabled') {
+    enableVoiceMode();
+}
+voiceTop.addEventListener("click", () => {
+    if (!voiceOn) {
+        enableVoiceMode();
+        EnableAllSound();
+    } else {
+        disableVoiceMode();
+        DisableAllSound();
+
+    }
+
+    /*ifActiveVoice();*/
+});
+
+/* ********************************************************** */
+/* ########################################################## */
+/* Bootstrap Notify ***********************************************/
+$('.voiceOn').click(function () {
+    if ($(".voiceOn").hasClass("active")) {
+        console.log('Work');
+        $.notify({
+            // options
+            icon: 'bx bx-volume-full bx-sm',
+            title: 'Ovozli rejmda o’qish',
+            message: 'Siz matnni ovozli rejimda o’qishni yoqdingiz, kerakli matnni belgilang va paydo bo’lgan tugmachani bosing !',
+            // url: 'https://github.com/mouse0270/bootstrap-notify',
+            // target: '_blank'
+        }, {
+            // settings
+            element: 'body',
+            // position: null,
+            type: "info",
+            // allow_dismiss: true,
+            // newest_on_top: false,
+            showProgressbar: false,
+            placement: {
+                from: "top",
+                align: "right"
+            },
+            offset: 20,
+            spacing: 10,
+            z_index: 9999,
+            delay: 3500,
+            timer: 1000,
+            url_target: '_blank',
+            mouse_over: null,
+            animate: {
+                enter: 'animated fadeInDown',
+                exit: 'animated fadeOutRight'
+            },
+            onShow: null,
+            onShown: null,
+            onClose: null,
+            onClosed: null,
+            icon_type: 'class',
+            // template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+            //     '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+            //     '<span data-notify="icon"></span> ' +
+            //     '<span data-notify="title">{1}</span> ' +
+            //     '<span data-notify="message">{2}</span>' +
+            //     '<div class="progress" data-notify="progressbar">' +
+            //     '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+            //     '</div>' +
+            //     '<a href="{3}" target="{4}" data-notify="url"></a>' +
+            //     '</div>'
+        });
+    } else {
+        $.notify({
+            // options
+            icon: 'bx bx-volume-mute bx-sm',
+            title: 'Ovozli rejmda o’qish',
+            message: 'Siz matnni ovozli rejimda o’qishni o’chirdingiz !',
+            // url: 'https://github.com/mouse0270/bootstrap-notify',
+            // target: '_blank'
+        }, {
+            // settings
+            element: 'body',
+            // position: null,
+            type: "info",
+            // allow_dismiss: true,
+            // newest_on_top: false,
+            showProgressbar: false,
+            placement: {
+                from: "top",
+                align: "right"
+            },
+            offset: 20,
+            spacing: 10,
+            z_index: 9999,
+            delay: 3500,
+            timer: 1000,
+            url_target: '_blank',
+            mouse_over: null,
+            animate: {
+                enter: 'animated fadeInDown',
+                exit: 'animated fadeOutRight'
+            },
+            onShow: null,
+            onShown: null,
+            onClose: null,
+            onClosed: null,
+            icon_type: 'class',
+            // template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+            //     '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+            //     '<span data-notify="icon"></span> ' +
+            //     '<span data-notify="title">{1}</span> ' +
+            //     '<span data-notify="message">{2}</span>' +
+            //     '<div class="progress" data-notify="progressbar">' +
+            //     '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+            //     '</div>' +
+            //     '<a href="{3}" target="{4}" data-notify="url"></a>' +
+            //     '</div>'
+        });
+    }
+})
+
+
+
 // Partners | Swiper ********************************** */
 if ($(".partnersSlider").length) {
     var swiper = new Swiper(".partnersSlider", {
